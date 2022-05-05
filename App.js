a35222c1cb8538f24b879b84a94620e74b3fd801
wwwwwmw <william.manwai@gmail.com>
2022-05-06 02:22:52 +0800

    completed

--- App.js
+++ App.js
@@ -1,12 +1,92 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,TextInput,Button ,SafeAreaView} from 'react-native';
+import React, { useState } from 'react';
 
 export default function App() {
+  // const [text, setText] = useState('');
+  const [text, onChangeText] = React.useState("Answer Goes Here");
+  const [numberA, onChangeNumberA] = React.useState(null);
+  const [numberB, onChangeNumberB] = React.useState(null);
+  const [numberC, onChangeNumberC] = React.useState(null);
+
+  const [titleText, setTitleText] = useState("The answer :");
+  const bodyText = "Click confirm to get the answer";
+
+  const onPressTitle = () => {
+    setTitleText("Click confirm to get the answer");
+  };
+
+  const showtheAns = () => {
+    const ans = Number(numberA) + Number(numberB) + Number(numberC)
+    console.log(numberA)
+    setTitleText(`The answer : ${ans}`)
+  };
+
+  const resetAns = () => {
+    onChangeNumberA(0)
+    onChangeNumberB(0)
+    onChangeNumberC(0)
+  }
+  const resetAll = () => {
+    onChangeNumberA(0)
+    onChangeNumberB(0)
+    onChangeNumberC(0)
+    setTitleText(`The answer : `)
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+    <SafeAreaView>
+          <Text style={styles.superTitleText} >
+            Comp 7506   Individual Assignment
+          </Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={onChangeNumberA}
+            value={numberA}
+            placeholder="Number 1"
+            keyboardType="numeric"
+          />
+          <TextInput
+            style={styles.input}
+            onChangeText={onChangeNumberB}
+            value={numberB}r
+            placeholder="Number 2"
+            keyboardType="numeric"
+          />
+          <TextInput
+            style={styles.input}
+            onChangeText={onChangeNumberC}
+            value={numberC}
+            placeholder="Number 3"
+            keyboardType="numeric"
+          />
+
+        <Text style={styles.baseText}>
+              <Text style={styles.titleText} 
+              // onPress={onPressTitle}
+              >
+                {titleText}
+                {"\n"}
+                {"\n"}
+              </Text>
+              <Text numberOfLines={5}>{bodyText}</Text>
+            </Text>
+    
+      
+
+      <Button
+        title="Confirm"
+        onPress={showtheAns } 
+      />
+      <Button
+        title="Clear Input"
+        onPress={resetAns} 
+      />
+      <Button
+        title="Reset"
+        onPress={resetAll} 
+      />
+      </SafeAreaView>
   );
 }
 
@@ -17,4 +97,25 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  }, 
+  baseText: {
+    margin: 12,
+    fontFamily: "Cochin"
+  },
+  superTitleText:{
+    margin: 12,
+    fontSize: 20,
+    fontWeight: "bold",
+    fontFamily: "Cochin"
+  },
+  titleText: {
+    margin: 12,
+    fontSize: 20,
+    fontWeight: "bold"
+  }
 });
